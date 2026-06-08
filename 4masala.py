be0f26437492de60d8b5df3384fcd071dae52c9f
Ilhomjon-developer <ilhomjon7937879@gmail.com>
2026-06-08 21:34:10 +0500

    String32 va String33

---
+++ 4masala.py
@@ -0,0 +1,9 @@
+#masala - 7
+def string_d(s1, s2):
+    return s1.count(s2)
+print(string_d("banana", "an"))
+
+#masala - 8
+def string_z(s1, s2):
+    return s1.replace(s2, "", 1)
+print(string_z("banana", "an"))
